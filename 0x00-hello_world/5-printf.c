02525b217c83700e1206bb6dc7d2c3c1418625da
Hassanayo <funyem1710@gmail.com>
2022-04-07 10:31:00 -0700

    low-level programming

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print func
+ * @void: no param
+ * Return: return string
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
