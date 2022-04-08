f4db95bcfbcd69e6371de5d5de03be2b1b8c583d
Hassanayo <funyem1710@gmail.com>
2022-04-08 08:38:09 -0700

    variable_if_else_while

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main func
+ * Return: return 0
+ */
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar(10);
+	return (0);
+}
