1443e64dad0b0401d1a71e05964d31bb3e1d517e
Hassanayo <funyem1710@gmail.com>
2022-04-08 09:17:41 -0700

    variable_if_else_while

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - main func
+ * Return: return 0
+ */
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		i++;
+	}
+	putchar(10);
+	return (0);
+}
