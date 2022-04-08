2e3741cd546f0571a4cd335601f14e6eb334eb47
Hassanayo <funyem1710@gmail.com>
2022-04-08 12:55:17 -0700

    variable_if_else_while

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - return main
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i,j,k;
+	for (i = 48; i < 58; i++)
+	{
+		for (j = 49; j < 58; j++)
+		{
+			for (k = 50; k < 58; k++)
+			{
+				if(k > j && j > i)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (i != 55 || j != 56)
+					{
+						putchar(44);
+						putchar(32);
+					}
+				}
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
