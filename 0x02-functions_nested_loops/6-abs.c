4b4ddb9956842ea35ee60ee8b36bead163714d95
Hassanayo <funyem1710@gmail.com>
2022-04-12 17:59:11 -0700

    functions_nested_loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _abs - function
+ * @n: integer
+ * Return: integer
+ */
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
