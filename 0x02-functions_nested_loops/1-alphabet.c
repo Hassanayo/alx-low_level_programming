f85aa97f4c5a4bcc54ed3eb5debc638e503c0cfa
Hassanayo <funyem1710@gmail.com>
2022-04-12 17:21:56 -0700

    functions_nested_loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - main func
+ * Return: return 0
+ */
+void print_alphabet_x10(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar(\'n');
+}
