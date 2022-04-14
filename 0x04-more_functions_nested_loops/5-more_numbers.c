89861df3bab196c471f1a174446a818f4fda59ff
Hassanayo <funyem1710@gmail.com>
2022-04-14 08:28:36 -0700

    functions_nested_loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,16 +6,16 @@
 
 void more_numbers(void)
 {
-	int c, rpt;
+	int i, ch;
 
-	for (rpt = 0; rpt < 10; rpt++)
+	for (i < 0; i < 10; i++)
 	{
-		for (c = 0; c < 15; c++)
+		for (ch = 0; ch < 15; ch++)
 		{
-			if (c >= 10)
-				_putchar((c / 10) + 48);
-			_putchar((c % 10) + 48);
+			if (ch >= 10)
+				_putchar((ch / 10) + 48);
+			_putchar((ch % 10) + 48);
 		}
 		_putchar('\n');
-	}	
+	}
 }
