95aaa24731cc53a2d005d3e637fd84efd0af3649
Hassanayo <funyem1710@gmail.com>
2022-04-14 08:52:59 -0700

    functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_diagonal - print a line in the terminal
+ * @n: integer
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int line = 0;
+	int spaces;
+
+	if (n > 0)
+	{
+		while (line < n)
+		{
+			for (spaces = 0; spaces < line; spaces++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+			line++;
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
