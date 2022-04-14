13a3d6172f6749d6287796482164a3d8a2fc44b8
Hassanayo <funyem1710@gmail.com>
2022-04-14 07:51:22 -0700

    functions_nested_loops

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,11 +3,12 @@
  * print_numbers - print numbers from 0 to 9
  * Return: void
  */
+
 void print_numbers(void)
 {
 	int num;
 
-	for (num = 0; num <= 9; num++)
+	for (num = '0'; num <= '9'; num++)
 	{
 		_putchar(num);
 	}
