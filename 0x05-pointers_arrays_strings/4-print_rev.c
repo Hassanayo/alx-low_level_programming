ea5d32b59eae0393fe15b276a94d2755ca56df0e
Hassanayo <funyem1710@gmail.com>
2022-04-20 16:21:01 -0700

    Pointers,arrays and strings

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,23 +6,17 @@
  */
 void print_rev(char *s)
 {
-	int i, max, half;
-	char first, last;
-
-	i = 0;
+	int i = 0;
 
 	while (s[i] != '\0')
 	{
 		i++;
 	}
-	max = i - 1;
-	half = max / 2;
-	while (half >= 0)
+	i--;
+	while (i >= 0)
 	{
-		first = s[max - half];
-		last = s[half];
-		s[half] = first;
-		s[max - half] = last;
-		half--;
+		_putchar(s[i]);
+		i--;
 	}
+	_putchar('\n');
 }
