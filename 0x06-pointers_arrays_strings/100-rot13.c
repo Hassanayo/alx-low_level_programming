b163f33072e1dbb1a0105519a8b31293cc142749
Hassan Ayomide <funyem1710@gmail.com>
2022-04-24 20:39:40 +0100

    advanced tasks

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * *rot13 - changes to rot13
+ * @*s: word to be changed
+ *
+ * Return: returns the encoded string
+ */
+char *rot13(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		while ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
+		{
+			if ((s[i] > 'm' && s[i] <= 'z') || (s[i] > 'M' && s[i] <= 'Z'))
+			{
+				s[i] -= 13;
+				break;
+			}
+			s[i] += 13;
+			break;
+		}
+		i++;
+	}
+
+	return (s);
+}
