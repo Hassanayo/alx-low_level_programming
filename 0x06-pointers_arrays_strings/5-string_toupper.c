6f1c2b55aab8e03283f7e0eab469faa2482f11a2
Hassan Ayomide <funyem1710@gmail.com>
2022-04-24 20:19:18 +0100

    capitalized string

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * string_toupper - changes all lower case letters to uppercase letters
+ * @n: string
+ * Return: string
+ */
+char *string_toupper(char *n)
+{
+	int i;
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+			n[i] = n[i] - 32;
+	}
+	return (n);
+}
