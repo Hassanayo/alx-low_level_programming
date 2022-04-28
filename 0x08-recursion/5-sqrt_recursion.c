3aa9d5b4c49db97a65324773ee6f836d7c7467f9
Hassan Ayomide <funyem1710@gmail.com>
2022-04-28 22:50:57 +0100

    root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - square root
+ * @n - int
+ * Return: squareroot of the number
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (squrt(n, (n + 1)/2));
+	}
+}
+/**
+ * squrt - checks if perfect square
+ * @n: input
+ * @i: counter
+ * Return: if square root
+ */
+int squrt(int n, int i)
+{
+	if (i < 1)
+		return (-1);
+	else if (i * i == n)
+		return (i);
+	else
+		return (squareroot(n, i - 1));
+}
