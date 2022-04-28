e1fa2597c5836e64eb4da73733bf5966c8f44ac4
Hassan Ayomide <funyem1710@gmail.com>
2022-04-28 22:24:54 +0100

    pow recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+ /**
+  * _pow_recursion: pow recursion
+  * @x: int
+  * @y: int
+  * Return: riased to pwer
+  */
+int _pow_recursion(int x, int y)
+{
+        if (y == 0)
+                return (1);
+        else if (y < 0)
+                return (-1);
+        return (x * (_pow_recursion(x, (y-1))));
+}
