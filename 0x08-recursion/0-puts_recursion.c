30c4178173ba9df85bf9d316872c98be80eff941
Hassan Ayomide <funyem1710@gmail.com>
2022-04-28 19:54:39 +0100

    puts recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string
+ * @s: sreing char
+ * Return: a string
+ */
+void _puts_recursion(char *s)
+{
+	_puts_recursion(char *S);
+	_putchar('\n');
+}
