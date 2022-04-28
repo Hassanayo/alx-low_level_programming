1169b384e4b86d34cdce390e3e9fc91a42313939
Hassan Ayomide <funyem1710@gmail.com>
2022-04-28 21:57:13 +0100

    strlen

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen_recursion - length
+ * @s: str
+ * Return: length
+ */
+int _strlen_recursion(char *s)
+{
+	if(*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + _strlen_recursions(s + 1));
+	}
+}
