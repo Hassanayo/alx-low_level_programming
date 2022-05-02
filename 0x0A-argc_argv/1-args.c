cc5a5094d0aecee643a7647b1397deb745406052
Hassan Ayomide <funyem1710@gmail.com>
2022-05-02 23:47:03 +0100

    prnt num

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - main func
+ * @argc: argc
+ * @argv: argv
+ * Return: return int
+ */
+int main(int argc, char* argv[])
+{
+	printf("argc = %d\n", argc);
+	_putchar('\n');
+	return (0);
+}
