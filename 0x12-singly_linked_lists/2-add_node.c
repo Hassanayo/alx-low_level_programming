5440788b9f3da648fb5fe99c7ef143f9a10e6485
Hassan Ayomide <funyem1710@gmail.com>
2022-05-25 00:10:16 +0100

    add ndde

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,37 +1,37 @@
 #include "lists.h"
 #include <stdlib.h>
+
 /**
  * add_node - Adds a new node to the beggining of a linked list
  * @head: pointer to the head 
+ * @str: string to be added
  * Return:  address to the new elemnt else NULL
  */
 list_t *add_node(list_t **head, const char *str)
 {
-    char *dup;
-    int len;
-    list_t *n;
+	char *dup;
+	int len;
+	list_t *n;
 
-    n = malloc(sizeof(list_t));
-    if (n == NULL)
-    {
-        return (NULL);
-    }
-    dup = strdup(str);
-    if (dup == NULL)
-    {
-        free(n);
-        return (NULL);
-    }
-    for (len = 0; str[len];)
-    {
-        len++;
-    }
-    n->str = dup;
-    n->len = len;
-    n->next = *head;
+	n = malloc(sizeof(list_t));
+	if (n == NULL)
+		return (NULL);
 
-    *head = n;
+	dup = strdup(str);
+	if (dup == NULL)
+	{
+		free(n);
+		return (NULL);
+	}
 
-    return (n);
+	for (len = 0; str[len];)
+		len++;
 
+	n->str = dup;
+	n->len = len;
+	n->next = *head;
+
+	*head = n;
+
+	return (n);
 }
