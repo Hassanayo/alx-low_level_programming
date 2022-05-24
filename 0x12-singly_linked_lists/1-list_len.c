6e3f250fa89ad89c3f48027138e950e38640febf
Hassan Ayomide <funyem1710@gmail.com>
2022-05-24 23:40:41 +0100

    list length

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,9 +2,8 @@
 
 /**
  * list_len - lists the length of the list
- * @h - single linked list
+ * @h: single linked list
  * Return: number of elements in a linked list
- * 
  */
 size_t list_len(const list_t *h)
 {
@@ -15,6 +14,6 @@ size_t list_len(const list_t *h)
         n++;
         h = h->next;
     }
-    
+
     return (n);
 }
